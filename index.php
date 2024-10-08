f0a332074fd9b9802634d254f3d6c98fc43d3e51
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-08 16:41:41 +0200

    added content-disposition header

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php 
+// Import data with php method file_get_contents
+$data = file_get_contents('./src/dischi.json');
+// var_dump($data);
 
+//  (php manual) » Content-Disposition header to supply a recommended filename and force the browser to display the save dialog
+header('Content-Type: application/json');
+echo $data;
 ?>
